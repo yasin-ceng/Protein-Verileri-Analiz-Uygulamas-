88784ef17e7e94659d2d5d179c79199be1498941
yasin-ceng <59895745+yasin-ceng@users.noreply.github.com>
2021-11-22 12:57:05 +0300

    Add files via upload

---
+++ gorsel_odev1/gorsel_odev1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace gorsel_odev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_getir = new System.Windows.Forms.Button();
+            this.txt_in_prtnm = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_getir
+            // 
+            this.btn_getir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_getir.Location = new System.Drawing.Point(147, 233);
+            this.btn_getir.Name = "btn_getir";
+            this.btn_getir.Size = new System.Drawing.Size(193, 114);
+            this.btn_getir.TabIndex = 0;
+            this.btn_getir.Text = "Yeni Metni Kaydet";
+            this.btn_getir.UseVisualStyleBackColor = true;
+            this.btn_getir.Click += new System.EventHandler(this.btn_getir_Click);
+            // 
+            // txt_in_prtnm
+            // 
+            this.txt_in_prtnm.Location = new System.Drawing.Point(101, 161);
+            this.txt_in_prtnm.Multiline = true;
+            this.txt_in_prtnm.Name = "txt_in_prtnm";
+            this.txt_in_prtnm.Size = new System.Drawing.Size(295, 33);
+            this.txt_in_prtnm.TabIndex = 1;
+            this.txt_in_prtnm.Validating += new System.ComponentModel.CancelEventHandler(this.txt_in_prtnm_Validating);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(30, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(424, 25);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Düzenlemek İstediğiniz Altyazı Dosyası";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(73, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 20);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "i";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox1.BackgroundImage")));
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Silver;
+            this.groupBox1.Location = new System.Drawing.Point(855, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 347);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dosyanızı Buraya Sürükleyin";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(512, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(177, 342);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Location = new System.Drawing.Point(614, 424);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(75, 23);
+            this.btn_del.TabIndex = 7;
+            this.btn_del.Text = "Listeyi Sil";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(512, 424);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Seç Kaldır";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_getir;
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1277, 512);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_in_prtnm);
+            this.Controls.Add(this.btn_getir);
+            this.Name = "Form1";
+            this.Text = "Altyazı Edit Programı";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_getir;
+        private System.Windows.Forms.TextBox txt_in_prtnm;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_del;
+    }
+}
+
---
+++ gorsel_odev1/gorsel_odev1/Form1.cs
@@ -0,0 +1,492 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace gorsel_odev1
+{
+    public partial class Form1 : Form
+    {
+        private const char Separator = ' ';
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_getir_Click(object sender, EventArgs e)
+        {
+
+            for (int a = 0; a < listBox1.Items.Count; a++)
+            {
+                //if (txt_in_prtnm.Text != "")
+             
+                    pdbOkuBandirma(a);
+            }
+        }
+
+        private void pdbOkuBandirma(int a)
+        {
+            try
+            {
+                //Dosya yolu tanımlama satırları:
+
+                //string dosyaYoluCan = @"\\192.168.1.100\Portal\" + listBox1.Items[a].ToString();
+
+                string dosyaYoluCan = @"C:\Users\Yasin\Desktop\Portal\" + listBox1.Items[a].ToString();
+
+
+                //string dosyaYoluCan = @"\\192.168.1.100\Portal\" + txt_in_prtnm.Text + ".srt";
+
+
+
+                //dosyadan okunan satırları satir[] dizisine atar
+                string[] satir = System.IO.File.ReadAllLines(dosyaYoluCan);
+                // string[] temizSatir = null;
+                string temizSatir = null;
+                char[] ayracSpace = new char[] { ' ' };
+
+
+
+               
+                int n = 0;
+                int say = 0;
+
+
+ {
+
+
+
+
+                    for (int i = 0; i < satir.Length; i += 1)
+                    {
+                        // tekil += 3;
+                        if (i + 1 < satir.Length)
+                        {
+                            /*
+                            if( (!(satir[i].StartsWith("0"))) && (!(satir[i].StartsWith("1"))) && (!(satir[i].StartsWith("2")))
+                                && (!(satir[i].StartsWith("3"))) && (!(satir[i].StartsWith("4"))) && (!(satir[i].StartsWith("5")))
+                                && (!(satir[i].StartsWith("6"))) && (!(satir[i].StartsWith("7"))) && (!(satir[i].StartsWith("8")))
+                                && (!(satir[i].StartsWith("9"))) && (!(satir[i].StartsWith((i-1).ToString()))))*/
+
+                            if((!(satir[i].StartsWith((i+1).ToString())) &&  (!(satir[i].StartsWith((i - 1).ToString()))) && (!(satir[i].StartsWith((i).ToString()))) &&  (!(satir[i].StartsWith("0"))) && (!(satir[i].StartsWith(" "))) &&   (!(satir[i].StartsWith("1")))) && (!(satir[i].StartsWith("2")))
+                                && (!(satir[i].StartsWith("3"))) && (!(satir[i].StartsWith("4"))) && (!(satir[i].StartsWith("5"))) && (!(satir[i].StartsWith("6")))
+                                && (!(satir[i].StartsWith("9"))) && (!(satir[i].StartsWith("8"))) && (!(satir[i].StartsWith("7"))))
+                                temizSatir = temizSatir + " " +satir[i]  ;
+                            if (i > 150 && i < 155)
+                                temizSatir = temizSatir + "\n";
+                            n += 1;
+                        }
+
+                    }
+
+
+
+                    /*
+
+
+                    for (int i = 2; i < satir.Length; i += 4)
+                        {
+                            // tekil += 3;
+                            if (i + 1 < satir.Length)
+                            {
+
+                                //listBox1.Items.Add(satir[i].Trim());
+                                temizSatir = temizSatir + satir[i] + " " + satir[i + 1];
+                                if (i > 150 && i < 155)
+                                    temizSatir = temizSatir + "\n";
+                                n += 1;
+                            }
+
+                        }
+
+
+                    */
+
+
+
+
+                    //mentindeki kelimeleri ayıklama
+                    string[] kelimeler = temizSatir.Split(' ');
+
+                    //başlık kelimelerini ayıklama
+                    string[] baslik = listBox1.Items[a].ToString().Split(' ');
+
+
+
+                    //kelime değişikliği
+                    for (int i=0;i<kelimeler.Length;i++)
+                    {
+
+                        if (kelimeler[i].Equals("videolar", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "makaleler";
+
+                        if (kelimeler[i].Equals("Videolarımız", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "makalelerimiz";
+
+                        if (kelimeler[i].Equals("Konuştuk", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "yazdık";
+
+                        if (kelimeler[i].Equals("izleyin", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "okuyun";
+
+                        if (kelimeler[i].Equals("bakın", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "okuyun";
+
+                        if (kelimeler[i].Equals("video", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "makale";
+
+                        if (kelimeler[i].Equals("videoda", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "makalede";
+
+                        if (kelimeler[i].Equals("canlı yayın", StringComparison.InvariantCultureIgnoreCase))
+                            kelimeler[i] = "yazı";
+
+                        if (kelimeler[i].Equals("hoşgeldiniz", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "Sevgili okurlar,";
+
+                        if (kelimeler[i].Equals("ağabey", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "sevgili okurlar";
+
+                        if (kelimeler[i].Equals("abi", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "sevgili okurlar";
+
+                        if (kelimeler[i].Equals("kardeş", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "sevgili okurlar";
+
+                        if (kelimeler[i].Equals("kardeşim", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "sevgili okurlar";
+
+                        if (kelimeler[i].Equals("arkadaşlar", StringComparison.InvariantCultureIgnoreCase))
+                        kelimeler[i] = "sevgili okurlar";
+
+                    }
+
+                    for (int i = 0; i < kelimeler.Length; i++)
+                    {
+                        for (int j = 0; j < baslik.Length; j++) {
+                            if (kelimeler[i].Equals(baslik[j], StringComparison.InvariantCultureIgnoreCase))
+                            kelimeler[i] = "<strong>" + kelimeler[i] + "</strong>";
+                        }
+
+
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        // çalışan paragraf for döngüsü ile tagler ve paragraflar ayrılır
+
+                        for (say = 150; say < kelimeler.Length; say++)
+                    {
+
+
+                        if (kelimeler[say].Contains(".")) {
+                            kelimeler[say] += "\n\n<h3>";
+                            say++;
+                            break; 
+                        
+                        }
+
+                    }
+
+                        for (; say < kelimeler.Length; say++)
+                    {
+                        if (kelimeler[say].Contains("."))
+                        {
+                            kelimeler[say] += "</h3>";
+                            say++;
+                            break;
+                        }
+                    }
+
+
+
+
+
+                    if(kelimeler.Length>450)
+                    for ( say = 450; say < kelimeler.Length; say++)
+                    {
+
+
+                        if (kelimeler[say].Contains("."))
+                        {
+                            kelimeler[say] += "\n\n<h2>";
+                                say++;
+                                break;
+                        }
+
+                    }
+
+                    if (kelimeler.Length > 450)
+                        for (; say < kelimeler.Length; say++)
+                    {
+                        if (kelimeler[say].Contains("."))
+                        {
+                            kelimeler[say] += "</h2>";
+                            break;
+                        }
+                    }
+
+
+
+
+                    if (kelimeler.Length > 650)
+                        for ( say = 650; say < kelimeler.Length; say++)
+                        {
+
+
+                            if (kelimeler[say].Contains("."))
+                            {
+                                kelimeler[say] += "\n\n<h2>";
+                                say++;
+                                break;
+                            }
+
+                        }
+
+                    if (kelimeler.Length > 650)
+
+                        for (; say < kelimeler.Length; say++)
+                    {
+                        if (kelimeler[say].Contains("."))
+                        {
+                            kelimeler[say] += "</h2>";
+                            break;
+                        }
+                    }
+
+
+
+                    if (kelimeler.Length > 900)
+                        for ( say = 900; say < kelimeler.Length; say++)
+                        {
+
+
+                            if (kelimeler[say].Contains("."))
+                            {
+                                kelimeler[say] += "\n\n<h2>";
+                                say++;
+                                break;
+                            }
+
+                        }
+
+                    if (kelimeler.Length > 900)
+
+                        for (; say < kelimeler.Length; say++)
+                    {
+                        if (kelimeler[say].Contains("."))
+                        {
+                            kelimeler[say] += "</h2>";
+                            break;
+                        }
+                    }
+
+                    if (kelimeler.Length > 1050)
+                        for ( say = 1050; say < kelimeler.Length; say++)
+                        {
+
+
+                            if (kelimeler[say].Contains("."))
+                            {
+                                kelimeler[say] += "\n\n<h3>";
+                                say++;
+                                break;
+                            }
+
+                        }
+
+                    if (kelimeler.Length > 1050)
+                        for (; say < kelimeler.Length; say++)
+                    {
+                        if (kelimeler[say].Contains("."))
+                        {
+                            kelimeler[say] += "</h3>";
+                            break;
+                        }
+                    }
+
+                    if (kelimeler.Length > 1200)
+
+                        if (kelimeler.Length > 1200)
+                        for ( say = 1200; say < kelimeler.Length; say++)
+                        {
+
+
+                            if (kelimeler[say].Contains("."))
+                            {
+                                kelimeler[say] += "\n\n<h3>";
+                                say++;
+                                break;
+                            }
+
+                        }
+
+                    if (kelimeler.Length > 1200)
+                        for (; say < kelimeler.Length; say++)
+                    {
+                        if (kelimeler[say].Contains("."))
+                        {
+                            kelimeler[say] += "</h3>";
+                            break;
+                        }
+                    }
+
+                    if (kelimeler.Length > 1350)
+                        for (say = 1350; say < kelimeler.Length; say++)
+                        {
+
+
+                            if (kelimeler[say].Contains("."))
+                            {
+                                kelimeler[say] += "\n\n<h3>";
+                                say++;
+                                
+                                break;
+
+                            }
+
+                        }
+                    if (kelimeler.Length > 1350)
+                        for (; say < kelimeler.Length; say++)
+                    {
+                        if (kelimeler[say].Contains("."))
+                        {
+                            kelimeler[say] += "</h3>";
+                            break;
+                        }
+                    }
+
+
+                    /*
+                    int say = 150;
+                    do
+                    {
+
+
+                        if (kelimeler[say].Contains("."))
+                            kelimeler[say] += "\n";
+                        say++;
+                    } while (kelimeler[say].Contains('.'));
+                    */
+
+
+
+
+
+
+                    string makale = null;
+                    for (int i = 0; i < kelimeler.Length; i++)
+                        makale = makale +" "+ kelimeler[i];
+
+
+                    //FileStream fs = new FileStream(@"D:\pdb\Deneme.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                   //FileStream fs = new FileStream(@"\\192.168.1.100\Portal\MAKALELER\" + listBox1.Items[a].ToString() + ".txt", FileMode.OpenOrCreate, FileAccess.Write);
+                    FileStream fs = new FileStream(@"C:\Users\Yasin\Desktop\Portal\MAKALELER\" + listBox1.Items[a].ToString() + ".txt", FileMode.OpenOrCreate, FileAccess.Write);
+
+
+                    StreamWriter sw = new StreamWriter(fs);
+                    sw.WriteLine(makale);
+                    sw.Flush();
+                    sw.Close();
+                    fs.Close();
+
+
+
+                }
+                }
+            catch (System.IO.IOException) //oluşması muhtemel hatalı dosya adı yakalanır
+            {
+                MessageBox.Show(txt_in_prtnm.Text + " Adında Dosya Bulunamadı");
+            }
+            catch (System.ArgumentException)  //dosya adı olamayacak veri girildiğinde hata yakalanır
+            {
+                MessageBox.Show("Geçersiz Karakter Girildi");
+            }
+        }
+
+        private void txt_in_prtnm_Validating(object sender, CancelEventArgs e)
+        {
+            if (txt_in_prtnm.Text.Trim() == "")
+                errorProvider1.SetError(txt_in_prtnm, "Dosya adını giriniz");
+            else
+                errorProvider1.SetError(txt_in_prtnm, "");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("sunucu\\ dizini altındaki dosyaları işleme alır.\n\n\"Girdi Örneği: 7buy\"  ");
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    e.Effect = DragDropEffects.Copy;
+                    
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None;
+                }
+
+
+        }
+
+
+
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+
+            //boşluk karakteri ayraç olarak tanımlandı:
+            char[] ayrac = new char[] { '.' };
+
+
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            //label1.Text = filePaths[0];
+            filePaths[0].Split(ayrac);
+//            label1.Text = a.ToString();
+            String dosyaSrt = filePaths[0].Substring(30);
+            listBox1.Items.Add(dosyaSrt);
+//           txt_in_prtnm.Text = dosyaSrt;
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            txt_in_prtnm.Text  =  listBox1.SelectedItem.ToString();
+        }
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+
+        }
+    }
+}
